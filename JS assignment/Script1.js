c3e1b4e0df35304f15c1a0681a6c008511dbd994
Muhammad Qasim <108570561+qmskipper@users.noreply.github.com>
2022-07-14 06:56:21 -0400

    Add files via upload

---
+++ JS assignment/Script1.js
@@ -0,0 +1,24 @@
+function FormValid(){
+    var Name = document.getElementById("Name");
+    var nameValue = Name.value;
+    if (nameValue == "") {
+        alert("wrong entry")
+    }
+       
+}
+function FormValid() {
+    var Email = document.getElementById("Email");
+    var emailValue = Email.value;
+    if (emailValue == "") {
+        alert("wrong entry")
+    }
+
+}
+function FormValid() {
+    var DOB = document.getElementById("DOB");
+    var DOCValue = DOB.value;
+    if (DOBlValue == "") {
+        alert("wrong entry")
+    }
+
+}
